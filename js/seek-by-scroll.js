42469a2c21a4ad3374991361e6d233f5ae5e3cdf
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-04-22 19:34:59 +0900

    動画再生の自動再開をオフにした。

--- js/seek-by-scroll.js
+++ js/seek-by-scroll.js
@@ -1,3 +1,25 @@
+(function() {
+    try {
+        console.log(
+                ' _   _  _____  __  __  _     ___    _       _    ___\n' +
+                '| |_| ||_   _||  \\/  || |   | __|  | |     / \\  | D )\n' +
+                '|  _  |  | |  | |\\/| || |__ |__ \\  | |__  / A \\ | D \\\n' +
+                '|_| |_|  |_|  |_|  |_||____||___/  |____|/_/¯\\_\\|___/\n' +
+                '\n' +
+                '         N T T   C o m m u n i c a t i o n s\n' +
+                '\n' +
+                'Hi! Thank you for looking in the developer console!\n' +
+                'If you have any cool ideas or see any bugs,\n' +
+                'please submit a pull request.\n' +
+                '\n' +
+                'コンソールまで見てくれてありがとう！\n' +
+                'おもしろいアイデアや不具合などあれば、プルリクエストを送ってくださいね。\n' +
+                '\n' +
+                'https://github.com/html5lab/html5lab.github.io/'
+        );
+    } catch (e) {}
+})();
+
 $(document).on('ready', function() {
     var $window = $(window);
     var $html = $('html');
@@ -12,70 +34,83 @@ $(document).on('ready', function() {
     var documentHeight = 0;
     var isPlaying = false;
     var isSuspended = false;
-    var SCROLL_DELAY_TIME = 5000;
     var IE_SEEK_INTERVAL = 1000;
-    var lastSeekTime = Date.now();
+    var lastSeekTime = 0;
+    var isWindowLoaded = false;
+    var isVideoLoaded = false;
 
     var readyHandler = function () {
-        resizeDelayHandeler();
+        console.log('document.onready');
     };
 
     var loadHandler = function() {
-        // document.height が変わるのでもう一度呼ぶ
+        console.log('window.onload');
+        isWindowLoaded = true;
+        if (isWindowLoaded && isVideoLoaded) {
+            initialize();
+        }
+    };
+
+    var loadedMetadataHandler = function() {
+        console.log('video.onloadedmetadata');
+        video.pause();
+
+        duration = video.duration;
+        var stringedDuration = (Math.floor(duration * 10) / 10).toFixed(1);
+        $('#duration').text(stringedDuration + 's');
+    };
+
+    var loadedDataHandler = function() {
+        console.log('video.onloadeddata');
+        isVideoLoaded = true;
+        if (isWindowLoaded && isVideoLoaded) {
+            initialize();
+        }
+    };
+
+    var initialize = function() {
         resizeDelayHandeler();
+        seekByScroll();
+        setTimeout(startPlaying, 0);
 
-        try {
-            console.log(
-                    ' _   _  _____  __  __  _     ___    _       _    ___\n' +
-                    '| |_| ||_   _||  \\/  || |   | __|  | |     / \\  | D )\n' +
-                    '|  _  |  | |  | |\\/| || |__ |__ \\  | |__  / A \\ | D \\\n' +
-                    '|_| |_|  |_|  |_|  |_||____||___/  |____|/_/¯\\_\\|___/\n' +
-                    '\n' +
-                    '         N T T   C o m m u n i c a t i o n s\n' +
-                    '\n' +
-                    'Hi! Thank you for looking in the developer console!\n' +
-                    'If you have any cool ideas or see any bugs,\n' +
-                    'please submit a pull request.\n' +
-                    '\n' +
-                    'コンソールまで見てくれてありがとう！\n' +
-                    'おもしろいアイデアや不具合などあれば、プルリクエストを送ってくださいね。\n' +
-                    '\n' +
-                    'https://github.com/html5lab/html5lab.github.io/'
-            );
-        } catch (e) {}
+        $(window)
+            .on('scroll', function() {
+                if (isPlaying) {
+                    console.log('ignored scroll event because isPlaying is true.');
+                    return;
+                }
+                if (video.readyState === video.HAVE_NOTHING) {
+                    console.log('ignored scroll event because of readyState is ' + video.readyState);
+                    return;
+                }
+                if (isSuspended) {
+                    console.warn('scroll event was fired though isSuspended is true.');
+                    return;
+                }
 
-        window.focus();
+                seekByScroll();
+            })
+            .on('resize', function() {
+                clearTimeout(resizeTimer);
+                resizeTimer = setTimeout(resizeDelayHandeler, 100);
+            })
     };
 
     var resizeDelayHandeler = function() {
         console.log('resizeDelayHandler was called.');
         windowHeight = $window.height();
-        $('.auto-height').css('min-height', windowHeight + 'px');
+        //$('.auto-height').css('min-height', windowHeight + 'px');
         currentScroll = $body.scrollTop() || $html.scrollTop();
         documentHeight = $document.height();
-        // console.log('documentHeight = ' + documentHeight + ', windowHeight = ' + windowHeight + ', currentScroll = ' + currentScroll)
-    };
-
-    var loadedMetadataHandler = function() {
-        console.log('loadedMetadataHandler');
-        video.pause();
-        duration = video.duration;
-        seekByScroll();
-    };
-
-    var loadedDataHandler = function() {
-        console.log('loadedDataHandler');
-        startPlayingTimer = setTimeout(startPlaying, SCROLL_DELAY_TIME);
-        var stringedDuration = (Math.floor(duration * 10) / 10).toFixed(1);
-        $('#duration').text(stringedDuration + 's');
+        //console.log('documentHeight = ' + documentHeight + ', windowHeight = ' + windowHeight + ', currentScroll = ' + currentScroll)
     };
 
     var seekByScroll = function() {
 
         // IEは頻繁にcurrentTimeを更新すると追随できないので、頻度を下げる
-        var isIE = !!navigator.userAgent.toLowerCase().match('msie 9');
+        var isIE9 = !!navigator.userAgent.toLowerCase().match('msie 9');
         var isVideoSupported = 'HTMLVideoElement' in window;
-        if (isIE && isVideoSupported) {
+        if (isIE9 && isVideoSupported) {
             var now = Date.now();
             if (now - lastSeekTime < IE_SEEK_INTERVAL) {
                 console.log('ignored seeking because of too short interval.');
@@ -110,16 +145,22 @@ $(document).on('ready', function() {
     };
 
     var startPlaying = function() {
+        if (video.seeking) {
+            console.log('skiped startPlaying');
+            setTimeout(startPlaying, 100);
+        }
         console.log('startPlaying');
         isPlaying = true;
+        $('html').addClass('isPlaying');
         video.play();
     };
 
     var stopPlaying = function() {
         console.log('stopPlaying');
-        $htmlAndBody.stop(true, true);
-        isPlaying = false;
         video.pause();
+        $htmlAndBody.stop(true, false);
+        $('html').removeClass('isPlaying');
+        isPlaying = false;
     };
 
     var visibilityChangeHandler = function() {
@@ -139,52 +180,14 @@ $(document).on('ready', function() {
             video.pause();
         }
 
-        if (isPlaying && !isSuspended) {
+        if (isPlaying && !isSuspended && !video.ended) {
             video.play();
-        }
-
-        if (!isPlaying && isSuspended) {
-            clearTimeout(startPlayingTimer);
-        }
-
-        if (!isPlaying && !isSuspended) {
-            if (!video.ended) {
-                startPlayingTimer = setTimeout(startPlaying, SCROLL_DELAY_TIME);
-            }
         }
     };
 
     var resizeTimer = null;
-    var startPlayingTimer = null;
 
-    $window
-        .on('load', loadHandler)
-        .on('scroll', function() {
-            if (isPlaying) {
-                console.log('ignored scroll event because isPlaying is true.');
-                return;
-            }
-            if (video.readyState === video.HAVE_NOTHING) {
-                console.log('ignored scroll event because of readyState is ' + video.readyState);
-                return;
-            }
-
-            seekByScroll();
-
-            if (isSuspended) {
-                console.warn('scroll event was fired though isSuspended is true.');
-                clearTimeout(startPlayingTimer);
-
-                // Suspend 中も seek はできるが、再生タイマーはセットしない。
-                return;
-            }
-            clearTimeout(startPlayingTimer);
-            startPlayingTimer = setTimeout(startPlaying, SCROLL_DELAY_TIME);
-        })
-        .on('resize', function() {
-            clearTimeout(resizeTimer);
-            resizeTimer = setTimeout(resizeDelayHandeler, 100);
-        });
+    $window.on('load', loadHandler);
 
     $video
         .on('loadedmetadata', loadedMetadataHandler)
@@ -200,7 +203,8 @@ $(document).on('ready', function() {
             }
             scrollByTimeUpdate();
         }).on('ended', function() {
-            isPlaying = false;
+            console.log('video ended.');
+            stopPlaying();
         });
 
     $document
@@ -212,9 +216,18 @@ $(document).on('ready', function() {
         })
         .on('visibilitychange', visibilityChangeHandler);
 
-    $('#playButton').on('click touchstart', function() {
-        alert();
-        startPlaying();
+    $('#playButton').on('click touchstart', function(event) {
+        event.preventDefault();
+        if (!isPlaying) {
+            startPlaying();
+        }
+    });
+
+    $('#pauseButton').on('click touchstart', function(event) {
+        event.preventDefault();
+        if (isPlaying) {
+            stopPlaying();
+        }
     });
 
     readyHandler();
