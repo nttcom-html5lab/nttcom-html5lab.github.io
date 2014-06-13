4f1bb2ac2ec7172c6bf6c9bc4442bf274596d33e
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-06-13 20:01:14 +0900

    consoleの有無をtry~catchではなく'console' in windowで判定

--- js/util.js
+++ js/util.js
@@ -7,7 +7,6 @@ var util = (function() {
         }
     };
 
-    try {
     var isIE9 = browser.ie && /^9/.test(browser.version);
     support.inlineVideo = support.video && !device.iPhone && !device.iPod && !isIE9;
     // iPhoneおよびiPod Touchは、videoのインライン再生が不可能なので、画像にフォールバックする
@@ -30,25 +29,26 @@ var util = (function() {
             });
         }
     }
+
+    if (support.console) {
         console.log(
-                '\n' +
-                ' _   _  _____  __  __  _     ___    _       _    ___\n' +
-                '| |_| ||_   _||  \\/  || |   | __|  | |     / \\  | D )\n' +
-                '|  _  |  | |  | |\\/| || |__ |__ \\  | |__  / A \\ | D \\\n' +
-                '|_| |_|  |_|  |_|  |_||____||___/  |____|/_/¯\\_\\|___/\n' +
-                '\n' +
-                '         N T T   C o m m u n i c a t i o n s\n' +
-                '\n' +
-                'Hi! Thank you for looking in the developer console!\n' +
-                'If you have any cool ideas or see any bugs,\n' +
-                'please submit a pull request.\n' +
-                '\n' +
-                'コンソールまで見てくれてありがとう！\n' +
-                'おもしろいアイデアや不具合などあれば、プルリクエストを送ってくださいね。\n' +
-                '\n' +
-                'https://github.com/html5lab/html5lab.github.io/'
-        );
-    } catch(e) {
+            '\n' +
+            ' _   _  _____  __  __  _     ___    _       _    ___\n' +
+            '| |_| ||_   _||  \\/  || |   | __|  | |     / \\  | D )\n' +
+            '|  _  |  | |  | |\\/| || |__ |__ \\  | |__  / A \\ | D \\\n' +
+            '|_| |_|  |_|  |_|  |_||____||___/  |____|/_/¯\\_\\|___/\n' +
+            '\n' +
+            '         N T T   C o m m u n i c a t i o n s\n' +
+            '\n' +
+            'Hi! Thank you for looking in the developer console!\n' +
+            'If you have any cool ideas or see any bugs,\n' +
+            'please submit a pull request.\n' +
+            '\n' +
+            'コンソールまで見てくれてありがとう！\n' +
+            'おもしろいアイデアや不具合などあれば、プルリクエストを送ってくださいね。\n' +
+            '\n' +
+            'https://github.com/html5lab/html5lab.github.io/'
+    );
     }
 
     return {
