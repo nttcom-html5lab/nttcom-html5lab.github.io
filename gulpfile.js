37fca883b6e1c42aae975919fca488fd48dd23bf
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-06-20 17:54:25 +0900

    gulpの最適化
    CSSのminifyには gulp-minify-css ではなく gul-cssmin を使う。
    bootstrap-html5lab.less は空っぽなので削除した。
    lessが1ファイルだけなので、CSSのconcatをやめる。
    gulp-coffee は不要。

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
 var gulp = require('gulp');
 
 var fs = require('fs');
-var less = require('gulp-less');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
-var concatCss = require('gulp-concat-css');
+var less = require('gulp-less');
+var cssMin = require('gulp-cssmin');
 
 var BUILD_DIR = './';
 
 var paths = {
     scripts: ['js/util.js', 'js/flexvideo.js', 'js/bgv.js', 'js/toppage.js'],
-    less: ['less/**/*.less'],
-    css: ['css/bootstrap-html5lab.css', 'css/html5lab.css']
+    less: ['less/**/*.less']
 };
 
 gulp.task('scripts', function() {
@@ -26,21 +24,14 @@ gulp.task('scripts', function() {
 gulp.task('less', function() {
     return gulp.src(paths.less)
         .pipe(less())
-        .pipe(gulp.dest(BUILD_DIR + '/css'));
-});
-
-gulp.task('css', function() {
-    return gulp.src(paths.css)
-        .pipe(minifyCss({keepBreaks: false}))
-        .pipe(concatCss('all.min.css'))
+        .pipe(cssMin())
         .pipe(gulp.dest(BUILD_DIR + '/css'));
 });
 
 gulp.task('watch', function() {
     gulp.watch(paths.scripts, ['scripts']);
     gulp.watch(paths.less, ['less']);
-    gulp.watch(paths.images, ['css']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['scripts', 'less', 'css', 'watch']);
+gulp.task('default', ['scripts', 'less', 'watch']);
